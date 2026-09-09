e39fd436222b686443491dc094ea13635c913083
Tobias Bohwalli <hi@futhr.io>
2026-09-09 19:10:26 +0200

    chore: configure release tooling

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule WotexContinuum.MixProject do
       wotex_dependency(),
       {:jason, "~> 1.4"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:git_ops, "~> 2.10", only: :dev, runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false},
       {:ex_check, "~> 0.16", only: [:dev, :test], runtime: false},
